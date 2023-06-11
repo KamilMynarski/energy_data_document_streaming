8388ab159e528d8c10e6a8c22c93922e5ccc2c41
kamilmynarski <kamilmynarski@gmail.com>
2023-06-11 10:36:50 +0200

    cleaning the code

--- client/api-client.py
+++ client/api-client.py
@@ -1,11 +1,8 @@
 import linecache
 import json
-
-# Make sure that requests is installed in your WSL
 import requests 
 
-# We could just read the entire file, but if it's really big you could go line by line
-# If you want make this an excercise and replace the process below by reading the whole file at once and going line by line
+
 
 #set starting id and ending id
 start = 1
@@ -18,10 +15,9 @@ while i <= end:
     
     # read a specific line
     line = linecache.getline('./client/output.txt', i)
-    #print(line)
+
     # write the line to the API
     myjson = json.loads(line)
-    
     
     response = requests.post('http://localhost:80/EnergyItem', json=myjson)
 
